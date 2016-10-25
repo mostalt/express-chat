061545276d0acb1cd59f5b67fb144193d65111b7
mostalt <antonov.a@wlbl.ru>
2016-10-25 16:38:00 +0300

    added views, data, mongodb native

--- app.js
+++ app.js
@@ -5,33 +5,82 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+//var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
-var app = express();
+var config = require('./config');
+var log = require('./libs/log')(module);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+var app = express();
+
+app.engine('ejs', require('ejs-locals')); //layout partial block
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+//Middlewares
+
+
+app.use(express.favicon());
+
+if (app.get('env') == 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.use(express.bodyParser()); //req.body...
+//app.use(express.methodOverride());
+app.use(express.cookieParser()); //req.cookies
+//app.use(express.session());
+app.use(app.router);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  if (req.url == '/') {
+    res.end('Home');
+  } else {
+    next();
+  }
+});
+
+app.use(function(req, res, next) {
+  if (req.url == '/forbidden') {
+    next(new Error('Oops, denied'));
+  } else {
+    next();
+  }
+});
+
+app.use(function(req, res) {
+  res.send(404, 'Page not found, sry bro')
+});
+
+app.use(function(err, req, res, next) {
+  //NODE_ENV = 'production'
+  if (app.get('env') == 'development') {
+    var errorHandler = express.errorHandler();
+    errorHandler(err, req, res, next);
+  } else {
+    res.send(500);
+  }
+});
+
+// development only
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());//
+//}
+
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + config.get('port'));
 });
---
+++ createDb.js
@@ -0,0 +1,30 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/chat';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    console.log(result);
+    callback(result);
+  });
+}
